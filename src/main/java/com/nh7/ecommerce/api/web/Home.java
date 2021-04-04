eb775d852db8e4b7447d36e8f147f0494e8a5b69
lamngochuy <lnhuy1912@gmail.com>
2021-04-04 17:53:14 +0700

    sss

--- src/main/java/com/nh7/ecommerce/api/web/Home.java
+++ src/main/java/com/nh7/ecommerce/api/web/Home.java
@@ -30,8 +30,11 @@ public class Home {
     public List<Product> getProductsByCategoryName(@PathVariable(value="category_name")String categoryName){
         return productService.getProductByCategoryName(categoryName);
     }
-    @PostMapping("/category")
-    public Object addNewCategory(@RequestBody Category category){
+    @GetMapping("/category/addnew")
+    public Object addNewCategory(){
+        Category category = new Category();
+        category.setCategoryName("duoc luon");
+        category.setCategoryThumbnail("ok chua");
         categoryService.saveCategory(category);
         return category;
     }
