34b545e464f47addc19d6f662bc4330da3d9fb89
lamngochuy <lnhuy1912@gmail.com>
2021-04-07 19:36:15 +0700

    aaa

--- src/main/java/com/nh7/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/nh7/ecommerce/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.nh7.ecommerce.repository;
 
 import com.nh7.ecommerce.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CategoryRepository extends CrudRepository<Category,Long> {
+public interface CategoryRepository extends JpaRepository<Category,Long> {
     Category findById(int id);
     @Query(value = "select * from category c where c.category_name like %:name%", nativeQuery = true)
     List<Category> findByCategoryName(@Param("name") String name);
