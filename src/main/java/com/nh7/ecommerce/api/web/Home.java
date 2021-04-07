f870fa13d2aa884bff466f971dbf36da79951ede
lamngochuy <lnhuy1912@gmail.com>
2021-04-07 19:29:04 +0700

    fff

--- src/main/java/com/nh7/ecommerce/api/web/Home.java
+++ src/main/java/com/nh7/ecommerce/api/web/Home.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@SpringBootApplication
+
 @RestController
 @RequestMapping("/home/api")
 public class Home {
